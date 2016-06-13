4c20f32153d527d09f647f1009d64afd30053fa6
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2016-06-13 11:28:37 +0200

    first commit

---
+++ Assets/Monitor Components/Demo/JumperMan.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumperMan : MonoBehaviour 
+{
+	private float horizontalInput;
+	private bool isGrounded;
+
+	void Awake()
+	{
+		gameObject.layer = 10;
+	}
+
+	void Update() 
+	{
+		horizontalInput = Input.GetAxis("Horizontal");
+		isGrounded = Physics.CheckSphere(transform.position, 0.8f, 1);
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		{
+			Debug.Log("Jump");
+
+			// apply jump velocity.
+			Vector3 velocity = GetComponent<Rigidbody>().velocity;
+			velocity.y = 10f;
+			GetComponent<Rigidbody>().velocity = velocity;
+		}
+
+		GetComponent<Rigidbody>().AddForce(10f * horizontalInput * Vector3.right * Time.deltaTime, ForceMode.VelocityChange);
+	}
+}
---
+++ Assets/MoveAlongSpline.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAlongSpline : MonoBehaviour {
+	public SplineComponent splineComponent;
+
+	public float currentPosition = 0;
+	public float speed = 1.0f;
+
+	public float currentSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 oldPos = transform.position;
+		var splineCurve = splineComponent.GetSplineObject();
+		Vector3 currentCurveVelocity = splineCurve.GetVelocity(currentPosition);
+		currentPosition += Time.deltaTime*speed / currentCurveVelocity.magnitude;
+		if (currentPosition > splineCurve.totalTime){
+			currentPosition = 0;
+		}
+		transform.position = splineCurve.GetPosition(currentPosition);
+		currentSpeed = (transform.position - oldPos).magnitude / Time.deltaTime;
+	}
+}
